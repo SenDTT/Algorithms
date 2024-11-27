28bd7d648fc6eb6b0111942fee7dcb6786d949c7
SenDoan <mariathanhsen@gmail.com>
2024-11-26 21:36:07 -0600

    Increasing Triplet Subsequence

--- LeetCode75/Medium/Array&String/IncreasingTripletSubsequence.ts
+++ LeetCode75/Medium/Array&String/IncreasingTripletSubsequence.ts
@@ -1,23 +1,20 @@
 // Increasing Triplet Subsequence
 function increasingTriplet(nums: number[]): boolean {
-  let j: number = 0;
-  let k: number = 0;
-  for (let i = 0; i < nums.length - 2; i++) {
-    j = i + 1;
-    while (j < nums.length - 1) {
-        if (nums[j] > nums[i]) {
-            k = j + 1;
-            while (k < nums.length && nums[k] <= nums[j]) k++;
-            console.log(i, j, k);
-            console.log(nums[i], nums[j], nums[k]);
-            if (k < nums.length && nums[j] < nums[k]) return true;
-        }
-        j++;
-    }
-  }
+    let first = Infinity;
+    let second = Infinity;
 
-  return false;
-}
+    for (let num of nums) {
+        if (num <= first) {
+            first = num;
+        } else if (num <= second) {
+            second = num;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+};
 
 console.log(increasingTriplet([1, 5, 0, 4, 1, 3])); // true
 console.log(increasingTriplet([20, 100, 10, 12, 5, 13])); // true
